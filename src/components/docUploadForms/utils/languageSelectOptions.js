b76166737a4ae851348ababf475d482524ecbe02
ECAPPZ <info@ecappz.in>
2025-01-17 15:36:21 +0530

    form utilities added

---
+++ src/components/docUploadForms/utils/languageSelectOptions.js
@@ -0,0 +1,526 @@
+export const LANGUAGE_OPTIONS = [
+  {
+    value: "Brazilian Portuguese",
+    label: "Brazilian Portuguese",
+  },
+  {
+    value: "Canadian French",
+    label: "Canadian French",
+  },
+  {
+    value: "English",
+    label: "English",
+  },
+  {
+    value: "French Creole",
+    label: "French Creole",
+  },
+  {
+    value: "Haitian Creole",
+    label: "Haitian Creole",
+  },
+  {
+    value: "Navajo",
+    label: "Navajo",
+  },
+  {
+    value: "Quechua",
+    label: "Quechua",
+  },
+  {
+    value: "Spanish",
+    label: "Spanish",
+  },
+  {
+    value: "Catalan",
+    label: "Catalan",
+  },
+  {
+    value: "Danish",
+    label: "Danish",
+  },
+  {
+    value: "Dutch",
+    label: "Dutch",
+  },
+  {
+    value: "Faroese",
+    label: "Faroese",
+  },
+  {
+    value: "Finnish",
+    label: "Finnish",
+  },
+  {
+    value: "Flemish",
+    label: "Flemish",
+  },
+  {
+    value: "French",
+    label: "French",
+  },
+  {
+    value: "German",
+    label: "German",
+  },
+  {
+    value: "Greek",
+    label: "Greek",
+  },
+  {
+    value: "Icelandic",
+    label: "Icelandic",
+  },
+  {
+    value: "Italian",
+    label: "Italian",
+  },
+  {
+    value: "Norwegian",
+    label: "Norwegian",
+  },
+  {
+    value: "Portuguese",
+    label: "Portuguese",
+  },
+  {
+    value: "Swedish",
+    label: "Swedish",
+  },
+  {
+    value: "UK English / British English",
+    label: "UK English / British English",
+  },
+  {
+    value: "Belarusian",
+    label: "Belarusian",
+  },
+  {
+    value: "Bosnian",
+    label: "Bosnian",
+  },
+  {
+    value: "Bulgarian",
+    label: "Bulgarian",
+  },
+  {
+    value: "Croatian",
+    label: "Croatian",
+  },
+  {
+    value: "Czech",
+    label: "Czech",
+  },
+  {
+    value: "Estonian",
+    label: "Estonian",
+  },
+  {
+    value: "Hungarian",
+    label: "Hungarian",
+  },
+  {
+    value: "Latvian",
+    label: "Latvian",
+  },
+  {
+    value: "Lithuanian",
+    label: "Lithuanian",
+  },
+  {
+    value: "Macedonian",
+    label: "Macedonian",
+  },
+  {
+    value: "Polish",
+    label: "Polish",
+  },
+  {
+    value: "Romanian",
+    label: "Romanian",
+  },
+  {
+    value: "Russian",
+    label: "Russian",
+  },
+  {
+    value: "Serbian",
+    label: "Serbian",
+  },
+  {
+    value: "Slovak",
+    label: "Slovak",
+  },
+  {
+    value: "Slovenian",
+    label: "Slovenian",
+  },
+  {
+    value: "Turkish",
+    label: "Turkish",
+  },
+  {
+    value: "Ukrainian",
+    label: "Ukrainian",
+  },
+  {
+    value: "Amharic (Ethiopia)",
+    label: "Amharic (Ethiopia)",
+  },
+  {
+    value: "Dinka (Sudan)",
+    label: "Dinka (Sudan)",
+  },
+  {
+    value: "Ibo (Nigeria)",
+    label: "Ibo (Nigeria)",
+  },
+  {
+    value: "Kirundi",
+    label: "Kirundi",
+  },
+  {
+    value: "Mandinka",
+    label: "Mandinka",
+  },
+  {
+    value: "Nuer (Nilo-Saharan)",
+    label: "Nuer (Nilo-Saharan)",
+  },
+  {
+    value: "Oromo (Ethiopia)",
+    label: "Oromo (Ethiopia)",
+  },
+  {
+    value: "Kinyarwanda",
+    label: "Kinyarwanda",
+  },
+  {
+    value: "Shona (Zimbabwe)",
+    label: "Shona (Zimbabwe)",
+  },
+  {
+    value: "Somali",
+    label: "Somali",
+  },
+  {
+    value: "Swahili",
+    label: "Swahili",
+  },
+  {
+    value: "Tigrigna (Ethiopia)",
+    label: "Tigrigna (Ethiopia)",
+  },
+  {
+    value: "Wolof",
+    label: "Wolof",
+  },
+  {
+    value: "Xhosa",
+    label: "Xhosa",
+  },
+  {
+    value: "Yoruba",
+    label: "Yoruba",
+  },
+  {
+    value: "Zulu",
+    label: "Zulu",
+  },
+  {
+    value: "Arabic",
+    label: "Arabic",
+  },
+  {
+    value: "Dari",
+    label: "Dari",
+  },
+  {
+    value: "Farsi",
+    label: "Farsi",
+  },
+  {
+    value: "Hebrew",
+    label: "Hebrew",
+  },
+  {
+    value: "Kurdish",
+    label: "Kurdish",
+  },
+  {
+    value: "Pashtu",
+    label: "Pashtu",
+  },
+  {
+    value: "Punjabi",
+    label: "Punjabi",
+  },
+  {
+    value: "Urdu (Pakistan)",
+    label: "Urdu (Pakistan)",
+  },
+  {
+    value: "Armenian",
+    label: "Armenian",
+  },
+  {
+    value: "Azerbaijani",
+    label: "Azerbaijani",
+  },
+  {
+    value: "Georgian",
+    label: "Georgian",
+  },
+  {
+    value: "Kazakh",
+    label: "Kazakh",
+  },
+  {
+    value: "Mongolian",
+    label: "Mongolian",
+  },
+  {
+    value: "Turkmen",
+    label: "Turkmen",
+  },
+  {
+    value: "Uzbek",
+    label: "Uzbek",
+  },
+  {
+    value: "Bengali",
+    label: "Bengali",
+  },
+  {
+    value: "Cham",
+    label: "Cham",
+  },
+  {
+    value: "Chamorro (Guam)",
+    label: "Chamorro (Guam)",
+  },
+  {
+    value: "Gujarati (India)",
+    label: "Gujarati (India)",
+  },
+  {
+    value: "Hindi",
+    label: "Hindi",
+  },
+  {
+    value: "Indonesian",
+    label: "Indonesian",
+  },
+  {
+    value: "Khmer (Cambodia)",
+    label: "Khmer (Cambodia)",
+  },
+  {
+    value: "Kmhmu (Laos)*",
+    label: "Kmhmu (Laos)*",
+  },
+  {
+    value: "Korean",
+    label: "Korean",
+  },
+  {
+    value: "Laotian",
+    label: "Laotian",
+  },
+  {
+    value: "Malayalam",
+    label: "Malayalam",
+  },
+  {
+    value: "Malay",
+    label: "Malay",
+  },
+  {
+    value: "Marathi (India)",
+    label: "Marathi (India)",
+  },
+  {
+    value: "Marshallese",
+    label: "Marshallese",
+  },
+  {
+    value: "Nepali",
+    label: "Nepali",
+  },
+  {
+    value: "Sherpa*",
+    label: "Sherpa*",
+  },
+  {
+    value: "Tamil",
+    label: "Tamil",
+  },
+  {
+    value: "Thai",
+    label: "Thai",
+  },
+  {
+    value: "Tibetan",
+    label: "Tibetan",
+  },
+  {
+    value: "Trukese (Micronesia)",
+    label: "Trukese (Micronesia)",
+  },
+  {
+    value: "Vietnamese",
+    label: "Vietnamese",
+  },
+  {
+    value: "Amoy",
+    label: "Amoy",
+  },
+  {
+    value: "Burmese",
+    label: "Burmese",
+  },
+  {
+    value: "Cantonese",
+    label: "Cantonese",
+  },
+  {
+    value: "Chinese",
+    label: "Chinese",
+  },
+  {
+    value: "Chinese–Simplified",
+    label: "Chinese–Simplified",
+  },
+  {
+    value: "Chinese–Traditional",
+    label: "Chinese–Traditional",
+  },
+  {
+    value: "Chiu Chow",
+    label: "Chiu Chow",
+  },
+  {
+    value: "Chow Jo",
+    label: "Chow Jo",
+  },
+  {
+    value: "Fukienese",
+    label: "Fukienese",
+  },
+  {
+    value: "Hakka (China)",
+    label: "Hakka (China)",
+  },
+  {
+    value: "Hmong",
+    label: "Hmong",
+  },
+  {
+    value: "Hainanese",
+    label: "Hainanese",
+  },
+  {
+    value: "Japanese",
+    label: "Japanese",
+  },
+  {
+    value: "Mandarin",
+    label: "Mandarin",
+  },
+  {
+    value: "Mien",
+    label: "Mien",
+  },
+  {
+    value: "Shanghainese",
+    label: "Shanghainese",
+  },
+  {
+    value: "Taiwanese",
+    label: "Taiwanese",
+  },
+  {
+    value: "Taishanese",
+    label: "Taishanese",
+  },
+  {
+    value: "Fijian",
+    label: "Fijian",
+  },
+  {
+    value: "Palauan",
+    label: "Palauan",
+  },
+  {
+    value: "Samoan",
+    label: "Samoan",
+  },
+  {
+    value: "Tongan",
+    label: "Tongan",
+  },
+  {
+    value: "Bikol",
+    label: "Bikol",
+  },
+  {
+    value: "Cebuano",
+    label: "Cebuano",
+  },
+  {
+    value: "Ilocano",
+    label: "Ilocano",
+  },
+  {
+    value: "Ilongo",
+    label: "Ilongo",
+  },
+  {
+    value: "Pampangan",
+    label: "Pampangan",
+  },
+  {
+    value: "Pangasinan",
+    label: "Pangasinan",
+  },
+  {
+    value: "Tagalog",
+    label: "Tagalog",
+  },
+  {
+    value: "Visayan",
+    label: "Visayan",
+  },
+  {
+    value: "American Sign Language",
+    label: "American Sign Language",
+  },
+  {
+    value: "Braille",
+    label: "Braille",
+  },
+  {
+    value: "Esperanto",
+    label: "Esperanto",
+  },
+  {
+    value: "Latin",
+    label: "Latin",
+  },
+  {
+    value: "Phonetic",
+    label: "Phonetic",
+  },
+  {
+    value: "Real Time & Remote Captioning",
+    label: "Real Time & Remote Captioning",
+  },
+  {
+    value: "Tactile",
+    label: "Tactile",
+  },
+  {
+    value: "Limited Resources Available",
+    label: "Limited Resources Available",
+  },
+];
