2ab25ffc5a6fb29cfb6dc15994cbe8d32021bcc2
ECAPPZ <info@ecappz.in>
2025-01-20 19:24:18 +0530

    Logic for display of doc upload form acc. to the case type. UI updated for no document upload

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,6 +1,7 @@
 import { Button, Modal } from "antd";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { UploadOutlined } from "@ant-design/icons";
 
 import StudyPermitExtension from "../components/docUploadForms/StudyPermitExtension";
 import StudySpouse from "../components/docUploadForms/StudySpouse";
@@ -20,16 +21,29 @@ const Document = () => {
 
   if (!lead) {
     return <div>Error 404</div>;
-  }
-  if (lead.Case_Type === "USA") {
+  } else {
     return (
       <div className="p-2">
         {docObj ? (
           <USADocumentView />
         ) : (
-          <div className="">
-            <div className="h-[40vh] flex justify-center items-center">
-              <Button type="primary" onClick={() => setOpen(true)}>
+          <>
+            <div className="flex h-[600px] mt-2 flex-col justify-center items-center">
+              <div>
+                <i className="bi bi-folder-x text-[100px] text-blue-600"></i>
+              </div>
+              <div className="font-semibold text-lg">
+                No Documents were uploaded
+              </div>
+              <div className="mt-3 text-sm">
+                There are no Documents uploaded as yet.
+              </div>
+              <Button
+                type="primary"
+                icon={<UploadOutlined />}
+                onClick={() => setOpen(true)}
+                className="mt-4"
+              >
                 Upload Documents
               </Button>
             </div>
@@ -37,13 +51,27 @@ const Document = () => {
             <Modal
               open={open}
               width={"80vw"}
-              footer={<></>}
+              footer={null}
               onClose={() => setOpen((curr) => !curr)}
               onCancel={() => setOpen((curr) => !curr)}
             >
-              <USA />
+              {lead.Case_Type === "Study Permit Extension" && (
+                <StudyPermitExtension />
+              )}
+              {lead.Case_Type === "Study Visa" && <StudyVisa />}
+              {lead.Case_Type === "Study+ Work Permit Extension" && (
+                <StudyWorkPermitExtension />
+              )}
+              {lead.Case_Type === "Study+Spouse" && <StudySpouse />}
+              {lead.Case_Type === "USA" && <USA />}
+              {lead.Case_Type === "Visitor to Study" && <VisitorStudies />}
+              {lead.Case_Type === "Visitor Visa" && <VisitorVisa />}
+              {lead.Case_Type === "Work Permit Extension" && (
+                <WorkPermitExtension />
+              )}
+              {lead.Case_Type === "Work Visa" && <WorkVisa />}
             </Modal>
-          </div>
+          </>
         )}
       </div>
     );
