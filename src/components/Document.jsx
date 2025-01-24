93c43e8afc6e14924d3d784c9f539776f2eeda7a
ECAPPZ <info@ecappz.in>
2025-01-24 20:02:13 +0530

    hidden zoho report header in Documents View

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -52,31 +52,31 @@ const Document = () => {
   const getEmbedCode = (case_type) => {
     switch (case_type) {
       case caseTypes.STUDY_PERMIT_EXTENSION:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Study_Permit_Extensions/ZGBbQZTNdFWEU9sngxmGpwU91QbHuD6sQMHpVEkp2fCG65BnP0x33m5f46KZt8C5E30m25ZURXEp8XU9O35qRdmujNhFbHqzbgpC?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Study_Permit_Extensions/ZGBbQZTNdFWEU9sngxmGpwU91QbHuD6sQMHpVEkp2fCG65BnP0x33m5f46KZt8C5E30m25ZURXEp8XU9O35qRdmujNhFbHqzbgpC?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.STUDY_VISA:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Study_Visa/DfQnd4UJdht3Wrrr9y8rAY3AC0pXXtTP6249R0DB7PF3sVfJsa0pVqH745nOwUdftgwDBY4GNvHPDQTgH3ORzxsVm7K0avDrZNKJ?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Study_Visa/DfQnd4UJdht3Wrrr9y8rAY3AC0pXXtTP6249R0DB7PF3sVfJsa0pVqH745nOwUdftgwDBY4GNvHPDQTgH3ORzxsVm7K0avDrZNKJ?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.STUDY_WORK_PERMIT_EXTENSION:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Study_Work_Permit_Extensions/qWfOsseKVe3bVdWq8p0J6j5RjGhszzAvAMpBkAkG34sfhbxRN7geesJpCC6vXmAGjYkY8bxtHhAVzA7U8muCNmhPzDbW5udMBz9s?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Study_Work_Permit_Extensions/qWfOsseKVe3bVdWq8p0J6j5RjGhszzAvAMpBkAkG34sfhbxRN7geesJpCC6vXmAGjYkY8bxtHhAVzA7U8muCNmhPzDbW5udMBz9s?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.STUDY_SPOUSE:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/Study_Spouse_Report/TquvmjdCXsYUyhbNgETuQ8z3kwfGKDaDTmdC2MZmZF35u7r5dUU8s8As66ney2MD7uy7erhdbKT0rx8Jg07WXwY6VOvyevsh69Oz?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/Study_Spouse_Report/TquvmjdCXsYUyhbNgETuQ8z3kwfGKDaDTmdC2MZmZF35u7r5dUU8s8As66ney2MD7uy7erhdbKT0rx8Jg07WXwY6VOvyevsh69Oz?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.USA:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Usa/njC7tm6yDusqm7DG07741jnU38AuPqqpvwRmjWKxzyf9QadzA4ZYKqhBCSk5J6wgb7JRbK8u2rACEh3pbOvdJ6WfqKOeJsJPQgX8?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Usa/njC7tm6yDusqm7DG07741jnU38AuPqqpvwRmjWKxzyf9QadzA4ZYKqhBCSk5J6wgb7JRbK8u2rACEh3pbOvdJ6WfqKOeJsJPQgX8?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.VISITOR_TO_STUDY:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Visitor_Studies/zHzPAsZ1hmm552uX5ejhezvrJuMka2PRB1wOZRMxt3kR55AYjUJVxfB1huNYYFx4jhSp4KSH4TJRhTZ49T0rEkr2PFYsbexnPMKO?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Visitor_Studies/zHzPAsZ1hmm552uX5ejhezvrJuMka2PRB1wOZRMxt3kR55AYjUJVxfB1huNYYFx4jhSp4KSH4TJRhTZ49T0rEkr2PFYsbexnPMKO?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.VISITOR_VISA:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Visitor_Visa/EhdWqfV2EXPCPhOrE4nNgaJWE3sD1HtV5OfBVXrYugDZp3B5h0Onz9ESz7V0nQS4B8MS9NKSa2nADBRXV9rGvrpJEYH1qOVYCD1J?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Visitor_Visa/EhdWqfV2EXPCPhOrE4nNgaJWE3sD1HtV5OfBVXrYugDZp3B5h0Onz9ESz7V0nQS4B8MS9NKSa2nADBRXV9rGvrpJEYH1qOVYCD1J?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.WORK_PERMIT_EXTENSION:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Work_Permit_Extensions/fyS2p7YjzZ44NB8JQ77qQvPmtaJ3VnmfTUUAb3yMnZ222YpVO1CTafbOpJyS6RfCa7WfCP1GkVpxkxCuaO9B8xjPrfNTXu8qhTpF?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Work_Permit_Extensions/fyS2p7YjzZ44NB8JQ77qQvPmtaJ3VnmfTUUAb3yMnZ222YpVO1CTafbOpJyS6RfCa7WfCP1GkVpxkxCuaO9B8xjPrfNTXu8qhTpF?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       case caseTypes.WORK_VISA:
-        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Work_Visa/j4YzA2yny7ZD3zAmO5ESGr1EF7U0RT2guC5EyhfzvMsUnHFs6XwNZkuxwD2GBfS4m1D5bEvV5x2GC1fq2gDMznKR4XrNSkOMAjgy?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
+        embedCode = `https://creatorapp.zohopublic.in/torontoimmigrationcompany/visa-management/report-embed/All_Work_Visa/j4YzA2yny7ZD3zAmO5ESGr1EF7U0RT2guC5EyhfzvMsUnHFs6XwNZkuxwD2GBfS4m1D5bEvV5x2GC1fq2gDMznKR4XrNSkOMAjgy?Lead.ID=${lead.ID}&hideColumns=Lead.ID;&zc_AddRec=false&zc_Search=false&zc_Header=false&zc_Footer=false&zc_ReportName=false&zc_ShowAs=false`;
         break;
       default:
         console.log("Error: Case Type and Report Name mismatch.");
